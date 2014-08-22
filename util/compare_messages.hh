12fd3a3da62e4a283d58448b477780f345545c89
David Beck <dbeck@starschema.net>
2014-08-22 23:19:51 +0200

    initlial steps for config_client and server; endpoint_client is now using 0MQ poll when listening to SUB channel; fix silly error in ip_discovery_server; added support for storing pb::ProcessInfo messages in std containers

--- util/compare_messages.hh
+++ util/compare_messages.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <svc_config.pb.h>
+#include <diag.pb.h>
 
 namespace virtdb { namespace util {
   
@@ -16,6 +17,35 @@ namespace virtdb { namespace util {
       
       if( lhs.svctype() < rhs.svctype() )
         return true;
+      else
+        return false;
+    }
+  };
+  
+  struct compare_process_info
+  {
+    bool operator()(const interface::pb::ProcessInfo & lhs,
+                    const interface::pb::ProcessInfo & rhs) const
+    {
+      if( lhs.startdate() < rhs.startdate() )
+        return true;
+      else if( lhs.startdate() > rhs.startdate() )
+        return false;
+      
+      if( lhs.starttime() < rhs.starttime() )
+        return true;
+      else if( lhs.starttime() > rhs.starttime() )
+        return false;
+      
+      if( lhs.pid() < rhs.pid() )
+        return true;
+      else if( lhs.pid() > rhs.pid() )
+        return false;
+      
+      if( lhs.random() < rhs.random() )
+        return true;
+      else if( lhs.random() > rhs.random() )
+        return false;
       else
         return false;
     }
