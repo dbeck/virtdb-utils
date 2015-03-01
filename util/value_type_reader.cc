428ea004630d78fbbf991329896469d05e43e3bf
David Beck <dbeck@starschema.net>
2015-03-02 00:30:33 +0100

    new shiny and fast value_type_reader added

---
+++ util/value_type_reader.cc
@@ -0,0 +1,157 @@
+#include "value_type_reader.hh"
+#include <util/exception.hh>
+#include <common.pb.h>
+
+namespace virtdb { namespace util {
+  
+  value_type_reader::value_type_reader(buffer && buf,
+                                       size_t len)
+  : buffer_{std::move(buf)},
+    is_((uint8_t *)buffer_.get(), len),
+    null_pos_{0}
+  {
+  }
+
+  value_type_reader::sptr
+  value_type_reader::construct(buffer && buf, size_t len)
+  {
+    value_type_reader::sptr ret;
+    std::vector<bool> nulls;
+    nulls.reserve(25000);
+    size_t start_pos = 0;
+    
+    uint32_t typ = 0;
+    {
+      // construct a temporary stream
+      stream_t tmp_is((uint8_t *)buf.get(), len);
+      
+      // read the type field's tag
+      auto tag = tmp_is.ReadTag();
+      
+      // read the type field
+      tmp_is.ReadVarint32(&typ);
+      
+      // check tag and type
+      if( tag != 1<<3 )
+      {
+        THROW_("invalid buffer, bad tag");
+      }
+      
+      if( typ < 2 || typ > 18 )
+      {
+        THROW_("invalid buffer, bad typ");
+      }
+      
+      bool null_ok = false;
+
+      auto read_nulls = [&]() {
+        if( null_ok ) return;
+        uint32_t payload = 0;
+        bool rv = tmp_is.ReadVarint32(&payload);
+        if( rv )
+        {
+          int pos = tmp_is.CurrentPosition();
+          int endpos = pos + payload;
+          while( pos < endpos )
+          {
+            uint32_t val = 0;
+            tmp_is.ReadVarint32(&val);
+            pos = tmp_is.CurrentPosition();
+            nulls.push_back(val!=0);
+          }
+        }
+        null_ok = true;
+      };
+      
+      start_pos = tmp_is.CurrentPosition();
+      
+      // next tag is either null's or ours
+      tag = tmp_is.ReadTag();
+      
+      // null tag came first
+      if( tag == ((11<<3)+2) )
+      {
+        read_nulls();
+        start_pos = tmp_is.CurrentPosition();
+        tag = tmp_is.ReadTag();
+      }
+      // bytes or strings
+      else if( tag == ((2<<3)+2) || tag == ((10<<3)+2) )
+      {
+        uint32_t ltag = tag;
+        while( true )
+        {
+          if( tag != ltag ) { break; }
+          uint32_t len = 0;
+          tmp_is.ReadVarint32(&len);
+          tmp_is.Skip(len);
+          tag = tmp_is.ReadTag();
+        }
+      }
+      // packed otherwise
+      else
+      {
+        uint32_t payload = 0;
+        tmp_is.ReadVarint32(&payload);
+        tmp_is.Skip(payload);
+        tag = tmp_is.ReadTag();
+      }
+      
+      if( tag == ((11<<3)+2) )
+      {
+        read_nulls();
+      }
+    }
+    
+    switch( typ )
+    {
+      case interface::pb::Kind::STRING:
+      case interface::pb::Kind::DATE:
+      case interface::pb::Kind::TIME:
+      case interface::pb::Kind::DATETIME:
+      case interface::pb::Kind::NUMERIC:
+      case interface::pb::Kind::INET4:
+      case interface::pb::Kind::INET6:
+      case interface::pb::Kind::MAC:
+      case interface::pb::Kind::GEODATA:
+        ret.reset(new vtr_impl::string_reader(std::move(buf),len,start_pos));
+        break;
+        
+      case interface::pb::Kind::INT32:
+        ret.reset(new vtr_impl::int32_reader(std::move(buf),len,start_pos));
+        break;
+        
+      case interface::pb::Kind::INT64:
+        ret.reset(new vtr_impl::int64_reader(std::move(buf),len,start_pos));
+        break;
+        
+      case interface::pb::Kind::BOOL:
+        ret.reset(new vtr_impl::bool_reader(std::move(buf),len,start_pos));
+        break;
+        
+      case interface::pb::Kind::UINT32:
+        ret.reset(new vtr_impl::uint32_reader(std::move(buf),len,start_pos));
+        break;
+        
+      case interface::pb::Kind::UINT64:
+        ret.reset(new vtr_impl::uint64_reader(std::move(buf),len,start_pos));
+        break;
+        
+      case interface::pb::Kind::DOUBLE:
+        ret.reset(new vtr_impl::double_reader(std::move(buf),len,start_pos));
+        break;
+        
+      case interface::pb::Kind::FLOAT:
+        ret.reset(new vtr_impl::float_reader(std::move(buf),len,start_pos));
+        break;
+        
+      case interface::pb::Kind::BYTES:
+        ret.reset(new vtr_impl::bytes_reader(std::move(buf),len,start_pos));
+        break;
+    };
+    
+    ret->nulls_.swap(nulls);
+    return ret;
+  }
+  
+}}
---
+++ util/value_type_reader.hh
@@ -0,0 +1,272 @@
+#pragma once
+
+#include <memory>
+#include <google/protobuf/io/coded_stream.h>
+#include <vector>
+#include <util/exception.hh>
+#include <common.pb.h>
+
+namespace virtdb { namespace util {
+  
+  class value_type_reader
+  {
+  public:
+    typedef std::shared_ptr<value_type_reader> sptr;
+    typedef std::unique_ptr<char[]>            buffer;
+    
+    enum status {
+      ok_,
+      type_mismatch_,
+      end_of_stream_
+    };
+    
+  private:
+    value_type_reader() = delete;
+    value_type_reader(const value_type_reader &) = delete;
+    value_type_reader & operator=(const value_type_reader&) = delete;
+    
+  protected:
+    typedef google::protobuf::io::CodedInputStream stream_t;
+    
+    value_type_reader(buffer && buf, size_t len);
+    
+    buffer             buffer_;
+    stream_t           is_;
+    size_t             null_pos_;
+    std::vector<bool>  nulls_;
+    
+  public:
+    static sptr construct(buffer && buf, size_t len);
+    
+    // all input types have a corresponding reader class
+    virtual inline status read_string(char ** ptr, size_t & len)  { return type_mismatch_; }
+    virtual inline status read_int32(int32_t & v)                 { return type_mismatch_; }
+    virtual inline status read_int64(int64_t & v)                 { return type_mismatch_; }
+    virtual inline status read_uint32(uint32_t & v)               { return type_mismatch_; }
+    virtual inline status read_uint64(uint64_t & v)               { return type_mismatch_; }
+    virtual inline status read_double(double & v)                 { return type_mismatch_; }
+    virtual inline status read_float(float & v)                   { return type_mismatch_; }
+    virtual inline status read_bool(bool & v)                     { return type_mismatch_; }
+    virtual inline status read_bytes(char ** ptr, size_t & len)   { return type_mismatch_; }
+    
+    inline size_t null_pos() const { return null_pos_; }
+    inline size_t n_nulls()  const { return nulls_.size(); }
+    
+    inline bool read_null()
+    {
+      bool ret = false;
+      if( null_pos_ < nulls_.size() )
+        ret = nulls_[null_pos_];
+      ++null_pos_;
+      return ret;
+    }
+    
+    virtual ~value_type_reader() {}
+  };
+  
+  namespace vtr_impl
+  {
+    template <typename T, uint32_t TAG>
+    class packed_reader : public value_type_reader
+    {
+    protected:
+      typedef T    data_t;
+      uint32_t     payload_;
+      int          endpos_;
+      
+      virtual inline status
+      read32(uint32_t & v)
+      {
+        int pos = is_.CurrentPosition();
+        if( pos < endpos_ )
+        {
+          is_.ReadVarint32(&v);
+          return ok_;
+        }
+        else
+        {
+          return end_of_stream_;
+        }
+      }
+
+      virtual inline status
+      read64(uint64_t & v)
+      {
+        int pos = is_.CurrentPosition();
+        if( pos < endpos_ )
+        {
+          is_.ReadVarint64(&v);
+          return ok_;
+        }
+        else
+        {
+          return end_of_stream_;
+        }
+      }
+      
+      virtual inline status
+      read(data_t & v)
+      {
+        int pos = is_.CurrentPosition();
+        if( pos < endpos_ )
+        {
+          is_.ReadRaw(&v, sizeof(data_t));
+          return ok_;
+        }
+        else
+        {
+          return end_of_stream_;
+        }
+      }
+      
+      packed_reader(buffer && buf, size_t len, size_t start_pos)
+      : value_type_reader(std::move(buf),len),
+        payload_{0},
+        endpos_{0}
+      {
+        is_.Skip(start_pos);
+        uint32_t tag = is_.ReadTag();
+        if( tag != TAG ) { return; }
+        is_.ReadVarint32(&payload_);
+        int pos = is_.CurrentPosition();
+        endpos_ = pos + payload_;
+      }
+      
+      virtual ~packed_reader() {}
+    };
+    
+    template <uint32_t TAG>
+    class buffer_reader : public value_type_reader
+    {
+    protected:
+      typedef std::pair<char *,size_t> data_t;
+      
+      virtual inline status
+      read(char ** ptr, size_t & rlen)
+      {
+        uint32_t tag = is_.ReadTag();
+        if( tag == TAG )
+        {
+          uint32_t len = 0;
+          is_.ReadVarint32(&len);
+          *ptr = buffer_.get()+is_.CurrentPosition();
+          rlen = len;
+          is_.Skip(len);
+          return ok_;
+        }
+        else
+        {
+          return end_of_stream_;
+        }
+      }
+      
+      buffer_reader(buffer && buf, size_t len, size_t start_pos)
+      : value_type_reader(std::move(buf),len)
+      {
+        is_.Skip(start_pos);
+      }
+      
+      virtual ~buffer_reader() {}
+    };
+    
+    class string_reader : public buffer_reader<((2<<3)+2)>
+    {
+    public:
+      typedef buffer_reader<((2<<3)+2)> parent_t;
+      virtual inline status read_string(char ** ptr, size_t & len) { return read(ptr,len); }
+      string_reader(buffer && buf, size_t len, size_t start_pos) : parent_t(std::move(buf),len,start_pos) {}
+      virtual ~string_reader() {}
+    };
+    
+    class int32_reader : public packed_reader<int32_t,((3<<3)+2)>
+    {
+    public:
+      typedef packed_reader<int32_t,((3<<3)+2)> parent_t;
+      virtual inline status read_int32(int32_t & v)
+      {
+        uint32_t n = 0;
+        auto ret = read32(n);
+        v = (n >> 1) ^ (-(n & 1));
+        return ret;
+      }
+      int32_reader(buffer && buf, size_t len, size_t start_pos) : parent_t(std::move(buf),len,start_pos) {}
+      virtual ~int32_reader() {}
+    };
+    
+    class int64_reader : public packed_reader<int64_t,((4<<3)+2)>
+    {
+    public:
+      typedef packed_reader<int64_t,((4<<3)+2)> parent_t;
+      virtual inline status read_int64(int64_t & v)
+      {
+        uint64_t n = 0;
+        auto ret = read64(n);
+        v = (n >> 1) ^ (-(n & 1));
+        return ret;
+      }
+      int64_reader(buffer && buf, size_t len, size_t start_pos) : parent_t(std::move(buf),len,start_pos) {}
+      virtual ~int64_reader() {}
+    };
+    
+    class uint32_reader : public packed_reader<uint32_t,((5<<3)+2)>
+    {
+    public:
+      typedef packed_reader<uint32_t,((5<<3)+2)> parent_t;
+      virtual inline status read_uint32(uint32_t & v) { return read32(v); }
+      uint32_reader(buffer && buf, size_t len, size_t start_pos) : parent_t(std::move(buf),len,start_pos) {}
+      virtual ~uint32_reader() {}
+    };
+    
+    class uint64_reader : public packed_reader<uint64_t,((6<<3)+2)>
+    {
+    public:
+      typedef packed_reader<uint64_t,((6<<3)+2)> parent_t;
+      virtual inline status read_uint64(uint64_t & v) { return read64(v); }
+      uint64_reader(buffer && buf, size_t len, size_t start_pos) : parent_t(std::move(buf),len,start_pos) {}
+      virtual ~uint64_reader() {}
+    };
+    
+    class double_reader : public packed_reader<double,((7<<3)+2)>
+    {
+    public:
+      typedef packed_reader<double,((7<<3)+2)> parent_t;
+      virtual inline status read_double(double & v) { return read(v); }
+      double_reader(buffer && buf, size_t len, size_t start_pos) : parent_t(std::move(buf),len,start_pos) {}
+      virtual ~double_reader() {}
+    };
+    
+    class float_reader : public packed_reader<float,((8<<3)+2)>
+    {
+    public:
+      typedef packed_reader<float,((8<<3)+2)> parent_t;
+      virtual inline status read_float(float & v) { return read(v); }
+      float_reader(buffer && buf, size_t len, size_t start_pos) : parent_t(std::move(buf),len,start_pos) {}
+      virtual ~float_reader() {}
+    };
+    
+    class bool_reader : public packed_reader<bool,((9<<3)+2)>
+    {
+    public:
+      typedef packed_reader<bool,((9<<3)+2)> parent_t;
+      virtual inline status read_bool(bool & v)
+      {
+        uint32_t v32 = 0;
+        auto ret = read32(v32);
+        v = (v32 != 0);
+        return ret;
+      }
+      bool_reader(buffer && buf, size_t len, size_t start_pos) : parent_t(std::move(buf),len,start_pos) {}
+      virtual ~bool_reader() {}
+    };
+    
+    class bytes_reader : public buffer_reader<((10<<3)+2)>
+    {
+    public:
+      typedef buffer_reader<((10<<3)+2)> parent_t;
+      virtual inline status read_bytes(char ** ptr, size_t & len) { return read(ptr,len); }
+      bytes_reader(buffer && buf, size_t len, size_t start_pos) : parent_t(std::move(buf),len,start_pos) {}
+      virtual ~bytes_reader() {}
+    };
+  }
+  
+}}
