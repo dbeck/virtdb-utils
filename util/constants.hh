8e0a8b0041092ed2aa6c8dac1a68f27a61186915
David Beck <dbeck@starschema.net>
2015-03-04 15:53:54 +0100

    separate raw message handling ad parsing in sub_client

--- util/constants.hh
+++ util/constants.hh
@@ -1,8 +1,9 @@
 #pragma once
 
 namespace virtdb { namespace util {
-  static const unsigned long DEFAULT_TIMEOUT_MS = 1000;
-  static const unsigned long SHORT_TIMEOUT_MS = 100;
-  static const unsigned long MAX_SUBSCRIPTION_SIZE = 1024;
+  static const unsigned long DEFAULT_TIMEOUT_MS      = 1000;
+  static const unsigned long TINY_TIMEOUT_MS         = 20;
+  static const unsigned long SHORT_TIMEOUT_MS        = 100;
+  static const unsigned long MAX_SUBSCRIPTION_SIZE   = 1024;
 }}
 
