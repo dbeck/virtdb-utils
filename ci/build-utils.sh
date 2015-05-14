7d06cf8b41c794e397820383eb70880892a7526c
David Beck <dbeck@starschema.net>
2015-05-14 15:57:15 +0200

    integrating jenkins ci

---
+++ ci/build-utils.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if [ "X" == "X$GITHUB_USER" ]; then echo "Need GITHUB_USER environment variable"; exit 10; fi
+if [ "X" == "X$GITHUB_PASSWORD" ]; then echo "Need GITHUB_PASSWORD environment variable"; exit 10; fi
+if [ "X" == "X$GITHUB_EMAIL" ]; then echo "Need GITHUB_EMAIL environment variable"; exit 10; fi
+if [ "X" == "X$HOME" ]; then echo "Need HOME environment variable"; exit 10; fi
+if [ "X" == "X$BUILDNO" ]; then echo "Need BUILDNO environment variable"; exit 10; fi
+
+COMPONENT=virtdb-utils
+
+cd $HOME
+
+git clone --recursive https://$GITHUB_USER:$GITHUB_PASSWORD@github.com/starschema/$COMPONENT.git 
+if [ $? -ne 0 ]; then echo "Failed to clone $COMPONENT repository"; exit 10; fi
+echo Creating build
+
+echo >>$HOME/.netrc
+echo machine github.com >>$HOME/.netrc
+echo login $GITHUB_USER >>$HOME/.netrc
+echo password $GITHUB_PASSWORD >>$HOME/.netrc
+echo >>$HOME/.netrc
+
+cd $HOME/$COMPONENT
+
+git --version
+git config --global push.default simple
+git config --global user.name $GITHUB_USER
+git config --global user.email $GITHUB_EMAIL
+
+gyp --depth=. -fmake 
+make 
+if [ $? -ne 0 ]; then echo "Failed to make $COMPONENT"; exit 10; fi
+
+echo $BUILDNO
+git tag -f $BUILDNO
+if [ $? -ne 0 ]; then echo "Failed to tag repo"; exit 10; fi
+git push origin $BUILDNO
+if [ $? -ne 0 ]; then echo "Failed to push tag to repo."; exit 10; fi
+
+out/Debug/utils-test
+if [ $? -ne 0 ]; then echo "$COMPONENT tests failed"; exit 10; fi
+
---
+++ ci/start-build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+REPO="git@github.com:starschema/virtdb-utils.git"
+BUILDNO=`git ls-remote --tags $REPO 2>&1 | ./new_build_tag.pl`
+export BUILDNO
+IMAGE_NAME="virtdb-build:centos6-utils-builder"
+cat Dockerfile.in | sed s/"@BUILDNO@"/$BUILDNO/g > Dockerfile
+docker build --force-rm=true -t "$IMAGE_NAME" .
+
+if [ $? -ne 0 ]
+then
+  echo "ERROR during docker build " $IMAGE_NAME 
+  exit 101
+fi
+
+echo "successfully built $IMAGE_NAME"
+
+mkdir -p build-result
+chmod a+rwxt build-result
+docker run --rm=true -e "GITHUB_EMAIL=$GITHUB_EMAIL" \
+                     -e "GITHUB_USER=$GITHUB_USER" \
+                     -e "GITHUB_PASSWORD=$GITHUB_PASSWORD" \
+                     -e "BUILDNO=$BUILDNO" \
+                     -v $PWD/build-result:/home/virtdb-demo/build-result -t $IMAGE_NAME ./build-utils.sh $*
+ls -ltr $PWD/build-result
+
